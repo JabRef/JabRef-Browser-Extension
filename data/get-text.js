1d124dcd06e7e31ff692065c42618a7147245298
Tobias Diez <tobiasdiez@gmx.de>
2016-02-13 15:57:02 +0100

    Reformat and clean code

--- data/get-text.js
+++ data/get-text.js
@@ -1,37 +1,49 @@
 var mainList = document.getElementById("itemList");
 var initalMessage = document.getElementById("initalMessage");
-//var attachmentsMap = new WeakMap();
 
+/*
+ * Show the item in the progress window.
+ */
 addon.port.on("show", function onShow(item) {
 
+	// Hide initial message
 	initalMessage.style.display = 'none';
 
+	// Create list entry for item
 	var listItem = document.createElement('li');
-	listItem.style.backgroundImage = "url('"+item.imageSrc+"')";
+	listItem.style.backgroundImage = "url('" + item.imageSrc + "')";
 	listItem.appendChild(document.createTextNode(item.title));
 
+	// Create sublist for attachments
 	var attachmentList = document.createElement('ul');
 	attachmentList.className = "attachmentList"
-	for(var i = 0; i < item.attachments.length; i++) {
+	for (var i = 0; i < item.attachments.length; i++) {
 		var attachmentItem = document.createElement('li');
 		attachmentItem.id = item.attachments[i].attachmentId;
 		attachmentItem.appendChild(document.createTextNode(item.attachments[i].title));
-		attachmentItem.style.backgroundImage = "url('"+item.attachments[i].imageSrc+"')";
+		attachmentItem.style.backgroundImage = "url('" + item.attachments[i].imageSrc + "')";
 		attachmentItem.style.opacity = 0.3;
 		attachmentList.appendChild(attachmentItem);
-    	}
+	}
 	listItem.appendChild(attachmentList);
 
 	mainList.appendChild(listItem);
 
-	addon.port.emit("winsize", {height: mainList.scrollHeight + 100, width: mainList.scrollWidth});
+	// Notify main code that the progress window should be resized
+	addon.port.emit("winsize", {
+		height: mainList.scrollHeight + 100,
+		width: mainList.scrollWidth
+	});
 });
 
+/*
+ * Update progress of attachment download.
+ */
 addon.port.on("updateProgress", function onUpdateProgress(item) {
 
 	var attachmentListItem = document.getElementById(item.attachmentId);
 	var progress = item.progress / 100;
-	if(progress < 0.3) 
+	if (progress < 0.3)
 		progress = 0.3;
 	attachmentListItem.style.opacity = progress;
 });
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var self = require('sdk/self');
 const {
-  Cc, Ci, Cu
+	Cc, Ci, Cu
 } = require("chrome");
 
 var Zotero = Cc["@zotero.org/Zotero;1"].getService(Ci.nsISupports).wrappedJSObject;
@@ -10,218 +10,232 @@ var tabs = require("sdk/tabs");
 var tab_utils = require("sdk/tabs/utils");
 var utils = require('sdk/window/utils');
 var {
-  viewFor
+	viewFor
 } = require("sdk/view/core");
-var { setTimeout } = require("sdk/timers");
+var {
+	setTimeout
+} = require("sdk/timers");
 var preferences = require("sdk/simple-prefs").prefs;
 
-// Add import button to Firefox toolbar.
+
+/*
+ * Add import button to Firefox toolbar.
+ */
 var importButton = buttons.ActionButton({
-  id: "import-button",
-  label: "Import references into JabRef",
-  icon: {
-    "16": "./JabRef-icon-16.png",
-    "48": "./JabRef-icon-48.png"
-  },
-  onClick: handleImportClick,
+	id: "import-button",
+	label: "Import references into JabRef",
+	icon: {
+		"16": "./JabRef-icon-16.png",
+		"48": "./JabRef-icon-48.png"
+	},
+	onClick: handleImportClick,
 });
 
 /*
  * Get the underlying document of the tab.
  */
 function getDocumentForTab(tab) {
-  var lowLevelTab = viewFor(tab);
-
-  // Get the ContentDocument of the tab
-  var browser = tab_utils.getBrowserForTab(lowLevelTab);
-  return browser.contentDocument;
+	var lowLevelTab = viewFor(tab);
+	// Get the ContentDocument of the tab
+	var browser = tab_utils.getBrowserForTab(lowLevelTab);
+	return browser.contentDocument;
 }
 
 /*
  * Import items found on the present website.
  */
 function handleImportClick(state) {
-  // Get active tab
-  var activeTab = tabs.activeTab;
-  var doc = getDocumentForTab(activeTab);
+	// Get active tab
+	var activeTab = tabs.activeTab;
+	var doc = getDocumentForTab(activeTab);
 
-  var panel = require("sdk/panel").Panel({
-    width: 330,
-    height: 150,
-    contentURL: "./text-entry.html"
-  });
+	// Create and show panel
+	var panel = require("sdk/panel").Panel({
+		width: 330,
+		height: 150,
+		contentURL: "./text-entry.html"
+	});
+	panel.port.on('winsize', function(data) {
+		panel.resize(330, data.height);
+	});
+	panel.show({
+		position: importButton
+	});
 
-  panel.port.on('winsize', function(data) {
-            panel.resize(330, data.height);
-  });
-
-  panel.show({
-      position: importButton
-    });
-
-  // Detect reference items
-  startImport(doc, panel);
+	// Detect and import bibliographic items
+	startImport(doc, panel);
 }
 
 /*
- * Sends all the contained reference items in doc to JabRef.
+ * Detects contained bibliographic items and sends them to JabRef.
  */
 function startImport(doc, panel) {
-  /*if (!(doc instanceof HTMLDocument))
-    return;
-  if (doc.documentURI.startsWith("about:"))
-    return;
-*/
-  // Search for translators
-  var translate = new Zotero.Translate.Web();
-  translate.setDocument(doc);
-  translate.setHandler("translators", function(obj, item) {
-    searchAndExportReferenceItems(obj, item, panel)
-  });
-  // false = only return one translator
-  translate.getTranslators(false);
+	// Look for translators which are able to handle the current website
+	var translate = new Zotero.Translate.Web();
+	translate.setDocument(doc);
+	translate.setHandler("translators", function(obj, item) {
+		detectAndExportReferenceItems(obj, item, panel)
+	});
+
+	translate.getTranslators(false); // false = return only the first translator
 }
 
 /*
- * Called when a translator search initiated with Zotero.Translate.getTranslators() is completed. Uses the found translator to search for items and export them.
+ * Called when a translator is found (search initiated by Zotero.Translate.getTranslators()).
+ * Uses the translator to search for bibliographic items and export them.
  */
-function searchAndExportReferenceItems(translate, translators, panel) {
-  var items = [];
-  translate.setTranslator(translators[0]);
+function detectAndExportReferenceItems(translate, translators, panel) {
+	var items = [];
+	translate.setTranslator(translators[0]);
 
-  translate.clearHandlers("itemDone");
-  translate.clearHandlers("done");
-  translate.clearHandlers("attachmentProgress");
-  
-  translate.setHandler("itemDone", function(obj, dbItem, item) {
-    var attachments = [];
-    for (var i = item.attachments.length - 1; i >= 0; i--) {
-      attachments.push({
-        attachmentId: JSON.stringify(item.attachments[i]).hashCode(), 
-        title: item.attachments[i].title, 
-        imageSrc: Zotero.Utilities.determineAttachmentIcon(item.attachments[i]),
-      });
-    };
-    var finishedItem = {title:item.title, attachments: attachments, imageSrc: Zotero.ItemTypes.getImageSrc(item.itemType)};
-    panel.port.emit("show", finishedItem);
-    items.push(dbItem);
-  });
-  translate.setHandler("done", function() {
-    exportItems(items);
-    
-    // Hide panel in 1 sec
-    setTimeout(function() {
-      panel.hide();
-    }, 1000);
-  });
-  translate.setHandler("attachmentProgress", function(obj, attachment, progress, error) {
-        panel.port.emit("updateProgress", {
-          attachmentId: JSON.stringify(attachment).hashCode(), 
-          progress: progress
-        });
-    });
+	translate.clearHandlers("itemDone");
+	translate.clearHandlers("done");
+	translate.clearHandlers("attachmentProgress");
 
-  translate.translate();
-  //translate.translate(false); // this would not save the items to the database
+	translate.setHandler("itemDone", function(obj, dbItem, item) {
+		// Prepare attachment data
+		var attachments = [];
+		for (var i = item.attachments.length - 1; i >= 0; i--) {
+			attachments.push({
+				attachmentId: JSON.stringify(item.attachments[i]).hashCode(),
+				title: item.attachments[i].title,
+				imageSrc: Zotero.Utilities.determineAttachmentIcon(item.attachments[i]),
+			});
+		};
+		var finishedItem = {
+			title: item.title,
+			attachments: attachments,
+			imageSrc: Zotero.ItemTypes.getImageSrc(item.itemType)
+		};
+		// Pass parsed item to progress window
+		panel.port.emit("show", finishedItem);
+
+		// Remember that this item was finished 
+		items.push(dbItem);
+	});
+
+	translate.setHandler("done", function() {
+		// Translation is done, so export the discovered items
+		exportItems(items);
+
+		// Hide panel in 1 sec
+		setTimeout(function() {
+			panel.hide();
+		}, 1000);
+	});
+
+	translate.setHandler("attachmentProgress", function(obj, attachment, progress, error) {
+		// Notify progress window about the attachment download
+		panel.port.emit("updateProgress", {
+			attachmentId: JSON.stringify(attachment).hashCode(),
+			progress: progress
+		});
+	});
+
+	// Run translator
+	translate.translate();
+	//translate.translate(false); // this would not save the items to the zotero database
 }
 
 /*
  * Exports the given items to JabRef.
  */
 function exportItems(items) {
-  var copyItems = items.slice(0);
-  if (items.length <= 0)
-    return;
+	if (items.length <= 0)
+		return;
 
-  // Create a new temporary file
-  var file = Cc["@mozilla.org/file/directory_service;1"].getService(Ci.nsIProperties).get("TmpD", Ci.nsIFile);
-  file.append("zotero_export.bib");
-  file.createUnique(Ci.nsIFile.NORMAL_FILE_TYPE, 0666);
+	// translate() deletes items array; this is why we save a cloned array here
+	var copyItems = items.slice(0);
 
-  var exportTranslator = new Zotero.Translate.Export();
-  //exportTranslator.setTranslator('9cb70025-a888-4a29-a210-93ec52da40d4'); // BibTeX
-  exportTranslator.setTranslator('b6e39b57-8942-4d11-8259-342c46ce395f'); // BibLaTeX
-  //exportTranslator.setItems([dbItem]);
-  exportTranslator.setItems(items);
-  exportTranslator.setLocation(file);
+	// Create a new temporary file
+	var file = Cc["@mozilla.org/file/directory_service;1"].getService(Ci.nsIProperties).get("TmpD", Ci.nsIFile);
+	file.append("zotero_export.bib");
+	file.createUnique(Ci.nsIFile.NORMAL_FILE_TYPE, 0666);
 
-  exportTranslator.clearHandlers("done");
-  exportTranslator.setHandler("done", function(obj, returnValue) {
-    importIntoJabRef(file);
-  });
+	// Prepare export
+	var exportTranslator = new Zotero.Translate.Export();
+	//exportTranslator.setTranslator('9cb70025-a888-4a29-a210-93ec52da40d4'); // BibTeX
+	exportTranslator.setTranslator('b6e39b57-8942-4d11-8259-342c46ce395f'); // BibLaTeX
+	exportTranslator.setItems(items);
+	exportTranslator.setLocation(file);
 
-  exportTranslator.translate();
-  // translate() deletes items array this is why we use the cloned array here
-  deleteItemsFromZoteroDatabase(copyItems);
+	exportTranslator.clearHandlers("done");
+	exportTranslator.setHandler("done", function(obj, returnValue) {
+		importIntoJabRef(file);
+	});
+
+	// Perform export
+	exportTranslator.translate();
+
+	// Delete saved items from zotero database, so we don't leave any traces
+	deleteItemsFromZoteroDatabase(copyItems);
 }
 
 /*
- * Imports the given bib file into JabRef.
+ * Sends the given bib file to JabRef.
  */
 function importIntoJabRef(file) {
-  var jabRef = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsILocalFile);
-  var process = Cc["@mozilla.org/process/util;1"].createInstance(Ci.nsIProcess);
-  var jabRefPath = preferences.jabRefPath;
-  jabRef.initWithPath(jabRefPath);
-  //jabRef.initWithPath("C:\\Program Files (x86)\\JabRef\\JabRef.exe");
-  //jabRef.initWithPath("C:\\Windows\\notepad.exe");
-  process.init(jabRef);
-
-  var args = ["--importToOpen", file.path];
-  //var args = [file.path];
-  process.run(false, args, args.length);
+	// Get JabRef executable
+	var jabRef = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsILocalFile);
+	jabRef.initWithPath(preferences.jabRefPath);
+	// Start JabRef
+	var process = Cc["@mozilla.org/process/util;1"].createInstance(Ci.nsIProcess);
+	process.init(jabRef);
+	var args = ["--importToOpen", file.path];
+	process.run(false, args, args.length);
 }
 
 /*
  * Delete items from Zotero db.
  */
 function deleteItemsFromZoteroDatabase(items) {
-  for (i = 0; i < items.length; i++) {
-    Zotero.Items.erase(items[i].id);
-  }
+	for (i = 0; i < items.length; i++) {
+		Zotero.Items.erase(items[i].id);
+	}
 }
 
 /*
- * Listen for tab content loads.
+ * Listen for tab content loads in order to enable/disable the import button.
  */
 tabs.on('ready', function(tab) {
-  var doc = getDocumentForTab(tab);
+	var doc = getDocumentForTab(tab);
 
-  // Search for translators
-  var translate = new Zotero.Translate.Web();
-  translate.setDocument(doc);
-  translate.setHandler("translators", function(obj, translators) {
-    if (!translators.length) {
-      // No translators found, so disable button
-      importButton.state(tab, {
-        disabled: true,
-        // We have to change the icon to gray since disabled=true does not gray-out the button (this is a bug https://bugzilla.mozilla.org/show_bug.cgi?id=1167559)
-        icon: {
-          "16": "./JabRef-icon-16-gray.png",
-          "48": "./JabRef-icon-48-gray.png"
-        },
-        label: "Import references into JabRef: no references found on website."
-      });
-    } else {
-      // Translators found, so update label
-      importButton.state(tab, {
-        label: "Import references into JabRef using " + translators[0].label
-      });
-    }
-  });
-  translate.getTranslators(false);
+	// Search for translators
+	var translate = new Zotero.Translate.Web();
+	translate.setDocument(doc);
+	translate.setHandler("translators", function(obj, translators) {
+		if (!translators.length) {
+			// No translators found, so disable button
+			importButton.state(tab, {
+				disabled: true,
+				// We have to change the icon to gray since disabled=true does not gray-out the button (this is a bug https://bugzilla.mozilla.org/show_bug.cgi?id=1167559)
+				icon: {
+					"16": "./JabRef-icon-16-gray.png",
+					"48": "./JabRef-icon-48-gray.png"
+				},
+				label: "Import references into JabRef: no references found on website."
+			});
+		} else {
+			// Translators found, so update label
+			importButton.state(tab, {
+				label: "Import references into JabRef using " + translators[0].label
+			});
+		}
+	});
+	translate.getTranslators(false);
 });
 
-
-// get hash code of string
-// from http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
-String.prototype.hashCode = function(){
-  var hash = 0;
-  if (this.length == 0) return hash;
-  for (i = 0; i < this.length; i++) {
-    char = this.charCodeAt(i);
-    hash = ((hash<<5)-hash)+char;
-    hash = hash & hash; // Convert to 32bit integer
-  }
-  return hash;
+/*
+ * Get hashcode of string
+ * Code from http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
+ */
+String.prototype.hashCode = function() {
+	var hash = 0;
+	if (this.length == 0) return hash;
+	for (i = 0; i < this.length; i++) {
+		char = this.charCodeAt(i);
+		hash = ((hash << 5) - hash) + char;
+		hash = hash & hash; // Convert to 32bit integer
+	}
+	return hash;
 }
