3c935bf858c47bb709aca78daf3a7252a163d886
Tobias Diez <tobiasdiez@gmx.de>
2017-10-14 12:22:22 +0800

    Add basic options page

--- options.js
+++ options.js
@@ -1,25 +1,21 @@
-"preferences": [{
-	"name": "jabRefPath",
-	"type": "file",
-	"title": "Path to JabRef",
-	"description": "The path of the executable file of JabRef, i.e. the install location of 'JabRef.exe'.",
-	"value": "C:\\Program Files\\JabRef\\JabRef.exe"
-}, {
-	"name": "exportMode",
-	"type": "menulist",
-	"title": "Export format",
-	"value": 0,
-	"options": [{
-		"value": "0",
-		"label": "BibLaTeX"
-	}, {
-		"value": "1",
-		"label": "BibTeX"
-	}]
-}, {
-	"name": "takeAutomaticSnapshots",
-	"type": "bool",
-	"title": "Automatically take snapshots",
-	"value": true,
-	"description": "Note: also changes the corresponding option for Zotero."
-}],
+function saveOptions(e) {
+	browser.storage.sync.set({
+		exportMode: document.querySelector("#exportMode").value,
+		takeAutomaticSnapshots: document.querySelector("#takeAutomaticSnapshots").checked
+	});
+	e.preventDefault();
+}
+
+function restoreOptions() {
+	browser.storage.sync.get({
+			exportMode: 0,
+			takeAutomaticSnapshots: false
+		})
+		.then(settings => {
+			document.querySelector("#exportMode").value = settings.exportMode;
+			document.querySelector("#takeAutomaticSnapshots").checked = settings.takeAutomaticSnapshots;
+		});
+}
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.querySelector("form").addEventListener("submit", saveOptions);
