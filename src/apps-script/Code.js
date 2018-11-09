JabRef/JabRef-Browser-Extension

commit 725e7b0bf5d893f4bb35bd590dd5893aeab05707

Speed up google docs updates by up to 60 times 🔥 🔥 🔥
Individual citation edits and updates should be a little faster,
but there is always a base cost to invoking Apps Script functions
so the speedup is more apparent when e.g. using delayed citation mode
and refreshing the doc, which requires updating every citation in the
doc

src/connector/client.js
@@ -46,7 +46,7 @@ Zotero.GoogleDocs = {
 	downloadIntercepted: false,
 
 	name: "Zotero Google Docs Plugin",
-	updateBatchSize: 8,
+	updateBatchSize: 32,
 	
 	init: async function() {
 		if (!await Zotero.Prefs.getAsync('integration.googleDocs.enabled')) return;
@@ -168,7 +168,7 @@ Zotero.GoogleDocs.Client.prototype = {
 		var keys = Object.keys(this.queued.fields); 
 		let batchSize = Zotero.GoogleDocs.updateBatchSize;
 		let count = 0;
-		while (count < keys.length) {
+		while (count < keys.length || this.queued.documentData) {
 			let batch = keys.slice(count, count+batchSize);
 			try {
 				await Zotero.GoogleDocs_API.run(this.documentID, 'complete', [
@@ -395,6 +395,7 @@ Zotero.GoogleDocs.Client.prototype = {
 		var endJSON = code.lastIndexOf('}');
 		if (startJSON != -1 && endJSON != -1) {
 			var json = JSON.parse(code.substring(startJSON, endJSON+1));
+			delete json.schema;
 			if (json.citationItems) {
 				for (let i = 0; i < json.citationItems.length; i++) {
 					delete json.citationItems[i].itemData.abstract;

src/apps-script/web-app.gs
@@ -413,7 +413,7 @@ exposed.insertField = function(field) {
 		});
 		return false; }
 
-	var namedRanges = encodeRange(bodyRange, field.code, config.fieldPrefix+field.id);
+	var namedRanges = encodeRange(getRangeFromLinks([link]), field.code, config.fieldPrefix+field.id);
 	return new Field(link, field.id, namedRanges, config.fieldPrefix).serialize();
 };
 
@@ -507,6 +507,8 @@ Field.prototype = {
 	 * @param field
 	 */
 	write: function(field) {
+		var newTextRange = null;
+		var newCode = (field.code && field.code != this.code) ? field.code : undefined;
 		if (field.text) {
 			var link = this.links[this.links.length-1];
 			var startOffset = link.startOffset;
@@ -520,7 +522,7 @@ Field.prototype = {
 			if (isBibl) {
 				paragraphModifiers = getBibliographyStyle();
 			}
-			HTMLConverter.insert(link.text, field.text, modifiers, startOffset, paragraphModifiers);
+			newTextRange = HTMLConverter.insert(link.text, field.text, modifiers, startOffset, paragraphModifiers);
 			
 			
 			// Remove old text
@@ -538,14 +540,21 @@ Field.prototype = {
 			// so we apply it one more time here
 			paragraphModifiers && this.links[this.links.length-1].text.getParent().setAttributes(paragraphModifiers);
 			
+			// Links no longer valid after inserting text
 			this.links = null;
+			// Inserting text removes the named ranges
+			// So if we were not planning to change the code we need to reinsert it
+			if (!newCode) {
+				newCode = this.code;
+			}
 		}
 		
-		if (field.code && this.code != field.code) {
+		if (newCode) {
 			this.namedRanges.forEach(function(namedRange) {
 				namedRange.remove();
 			});
-			this.namedRanges = encodeRange(bodyRange, field.code, config.fieldPrefix+this.id);
+			this.namedRanges = encodeRange(newTextRange || getRangeFromLinks(this.links), 
+				newCode, config.fieldPrefix+this.id);
 		}
 	},
 	
@@ -576,7 +585,7 @@ Field.prototype = {
 	"delete": function() {
 		this.link.text.deleteText(this.link.startOffset, this.link.endOffsetInclusive);
 		this.unlink();
-	}
+	},
 };
 
 // ----- UTILITIES ----- //
@@ -722,6 +731,15 @@ function iterateSections(doc, func) {
 	}
 }
 
+function getRangeFromLinks(links) {
+	var builder = doc.newRange();
+	for (var i = 0; i < links.length; i++) {
+		var link = links[i];
+		builder.addElement(link.text, link.startOffset, link.endOffsetInclusive);
+	}
+	return builder.build();
+}
+
 function getRangeLink(rangeElement) {
 	var elem = rangeElement.getElement();
 	if (elem.getType() != 'TEXT') {
@@ -772,6 +790,7 @@ function randomString(length) {
 }
 
 var HTMLConverter = {
+	startAt: 0,
 	insertAt: 0,
 	insertElem: null,
 	insertedLength: 0,
@@ -788,7 +807,7 @@ var HTMLConverter = {
 		this.parentElem = this.insertElem.getParent().getParent();
 		this.paragraphIndex = this.parentElem.getChildIndex(this.insertElem.getParent());
 		
-		this.insertAt = insertAt || 0;
+		this.insertAt = this.startAt = insertAt || 0;
 		// Might cause styling issues
 		this.defaultAttributes = this.insertElem.getAttributes();
 		// Preserve link
@@ -804,17 +823,16 @@ var HTMLConverter = {
 			this.insertElem.insertText(this.insertAt, html);
 			return html.length;
 		}
+		
+		this.rangeBuilder = doc.newRange();
 
 		// Insert formatted text
 		HTMLConverter.addElem(xmlDoc.getRootElement(), modifiers);
-
-		// Set params
-		// var fontSize = this.insertElem.getFontSize() || 11;
-		// params.linespacing && docsElem.setLineSpacing(params.linespacing);
-		// params.entryspacing && docsElem.setSpacingAfter(params.entryspacing * fontSize);
 		
-		// Return inserted text length
-		return this.insertedLength;
+		if (this.startAt != this.insertAt) {
+			this.rangeBuilder.addElement(this.insertElem, this.startAt, this.insertAt-1);
+		}
+		return this.rangeBuilder.build();
 	},
 
 	addElem: function(elem, modifiers) {
@@ -868,8 +886,11 @@ var HTMLConverter = {
 		else if (cls === 'csl-entry') {
 			// Don't insert the first paragraph, except when we're not in a new paragraph already
 			if (!this.firstParagraph || this.insertAt != 0) {
+				if (this.insertAt != this.startAt) {
+					this.rangeBuilder.addElement(this.insertElem, this.startAt, this.insertAt-1);
+				}
 				this.insertElem = this.parentElem.insertParagraph(this.paragraphIndex+1, "").editAsText();
-				this.insertAt = 0;
+				this.insertAt = this.startAt = 0;
 				this.paragraphIndex++;
 			}
 			if (this.paragraphModifiers) {

commit b0b856ac295bf7c545a5af1d30dd4959bca493d6

Fix note conversions (broken in 725e7b0b)

src/apps-script/Code.js
@@ -155,6 +155,18 @@ function getFields(prefix, removePlaceholder) {
 }
 
 /**
+ * To be used when converting and field codes are lost in the move between notes/body
+ */
+function getFieldPlaceholders() {
+	var fields = [];
+	filterFieldLinks(getAllLinks()).forEach(function(link) {
+		var key = link.url.substr(config.fieldURL.length, config.fieldKeyLength);
+		fields.push(new Field(link, key, [], config.fieldPrefix));
+	});
+	return fields;
+}
+
+/**
  * The idea here is to encode a field using the names of NamedRanges
  * https://developers.google.com/apps-script/reference/document/named-range
  * 
@@ -335,8 +347,13 @@ exposed.unlockTheDoc = function() {
 	doc.getNamedRanges(LOCK_NAME).forEach(function(r) {r.remove()});
 }
 
-exposed.getFields = function () {
-	var fields = getFields();
+exposed.getFields = function (placeholders) {
+	var fields;
+	if (placeholders) {
+		var fields = getFieldPlaceholders();
+	} else {
+		var fields = getFields();
+	}
 	fields = fields.map(function(field) {
 		return field.serialize();
 	});
@@ -346,27 +363,52 @@ exposed.getFields = function () {
 	return fields;
 };
 
-exposed.complete = function(insert, docPrefs, fieldChanges, bibliographyStyle, deletePlaceholder) {
+/**
+ * Function used to write changes to the document. Due to the slow nature of google docs
+ * we try to minimize calls to apps script and combine all doc updates in this function. 
+ * To prevent timeouts field changes may be batched in multiple calls, resulting in multiple
+ * calls of this function for a "single" integration operation
+ * 
+ * @param insert {Field}
+ * @param documentData {String}
+ * @param fields {Field[]}
+ * @param bibliographyStyle {Object}
+ * @param deletePlaceholder {Boolean}
+ * @param conversion {Boolean}
+ */
+exposed.complete = function(options) {
+	if (arguments.length > 1) {
+		options.insert = arguments[0];
+		options.documentData = arguments[1];
+		options.fields = arguments[2];
+		options.bibliographyStyle = arguments[3];
+		options.deletePlaceholder = arguments[4];
+	}
 	lockTheDoc();
 	try {
-		if (insert) {
-			exposed.insertField(insert);
+		if (options.insert) {
+			exposed.insertField(options.insert);
 		}
-		if (docPrefs) {
-			exposed.setDocumentData(docPrefs);
+		if (options.documentData) {
+			exposed.setDocumentData(options.documentData);
 		}
-		if (bibliographyStyle) {
-			exposed.setBibliographyStyle(JSON.stringify(bibliographyStyle));
+		if (options.bibliographyStyle) {
+			exposed.setBibliographyStyle(JSON.stringify(options.bibliographyStyle));
 		}
-
-		var fields = getFields(config.fieldPrefix, deletePlaceholder);
+		
+		var fields;
+		if (options.conversion) {
+			fields = getFieldPlaceholders();
+		} else {
+			fields = getFields(config.fieldPrefix, options.deletePlaceholder);
+		}
 		var fieldMap = {};
 		fields.forEach(function(field) {
 			fieldMap[field.id] = field;
 		});
 		var missingFields = [];
 		// Perform in reverse order to keep field link position indices intact during update
-		fieldChanges.reverse().forEach(function(fieldChange) {
+		options.fields.reverse().forEach(function(fieldChange) {
 			var field = fieldMap[fieldChange.id];
 			if (!field) {
 				missingFields.push(fieldChange.id);
@@ -411,9 +453,14 @@ exposed.insertField = function(field) {
 		error: new Error("Failed to insert field. Could not find the placeholder link.\n" + JSON.stringify(field)),
 		field: field
 		});
-		return false; }
+		return false;
+	}
 
-	var namedRanges = encodeRange(getRangeFromLinks([link]), field.code, config.fieldPrefix+field.id);
+	if (apiVersion >= 4) {
+		var namedRanges = encodeRange(getRangeFromLinks([link]), field.code, config.fieldPrefix+field.id);
+	} else {
+		var namedRanges = encodeRange(bodyRange, field.code, config.fieldPrefix+field.id);
+	}
 	return new Field(link, field.id, namedRanges, config.fieldPrefix).serialize();
 };
 
@@ -553,8 +600,13 @@ Field.prototype = {
 			this.namedRanges.forEach(function(namedRange) {
 				namedRange.remove();
 			});
-			this.namedRanges = encodeRange(newTextRange || getRangeFromLinks(this.links), 
-				newCode, config.fieldPrefix+this.id);
+			if (apiVersion >= 4) {
+				this.namedRanges = encodeRange(newTextRange || getRangeFromLinks(this.links),
+					newCode, config.fieldPrefix+this.id);
+			} else {
+				this.namedRanges = encodeRange(bodyRange,
+					newCode, config.fieldPrefix+this.id);
+			}
 		}
 	},
 	

src/connector/api.js
@@ -29,7 +29,7 @@ Zotero.GoogleDocs.API = {
 	authDeferred: null,
 	authHeaders: null,
 	lastAuthEmail: null,
-	apiVersion: 3,
+	apiVersion: 4,
 	
 	resetAuth: function() {
 		delete this.authHeaders;

src/connector/client.js
@@ -172,11 +172,10 @@ Zotero.GoogleDocs.Client.prototype = {
 			let batch = keys.slice(count, count+batchSize);
 			try {
 				await Zotero.GoogleDocs_API.run(this.documentID, 'complete', [
-					this.queued.insert,
-					this.queued.documentData,
-					batch.map(key => this.queued.fields[key]),
-					this.queued.bibliographyStyle,
-					count+batch < keys.length ? null : this.queued.deletePlaceholder
+					Object.assign({}, this.queued, {
+						fields: batch.map(key => this.queued.fields[key]),
+						deletePlaceholder: count+batch < keys.length ? null : this.queued.deletePlaceholder
+					})
 				]);
 			} catch(e) {
 				if (e.status == 429 || e.message.startsWith('Too many changes applied before saving document.')) {
@@ -242,7 +241,7 @@ Zotero.GoogleDocs.Client.prototype = {
 			return fields;
 		}
 
-		this.fields = await Zotero.GoogleDocs_API.run(this.documentID, 'getFields', Array.from(arguments));
+		this.fields = await Zotero.GoogleDocs_API.run(this.documentID, 'getFields', [this.queued.conversion]);
 		let i = 0;
 		for (let field of this.fields) {
 			if (field == -1) {
@@ -329,7 +328,8 @@ Zotero.GoogleDocs.Client.prototype = {
 		for (let field of fields) {
 			fieldMap[field.id] = field;
 		}
-		
+
+		this.queued.conversion = true;
 		if (fieldMap[fieldIDs[0]].noteIndex != fieldNoteTypes[0]) {
 			// Note/intext conversions
 			if (fieldNoteTypes[0] > 0) {
@@ -371,7 +371,6 @@ Zotero.GoogleDocs.Client.prototype = {
 					fieldIDs,
 				]);
 			}
-			delete this.fields;
 		}
 	},
 	