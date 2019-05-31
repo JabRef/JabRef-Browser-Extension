JabRef/JabRef-Browser-Extension

commit 117206a62d33b92c462f58e7a0d5d5ddb55ab4a1

Export and import citations in reverse order
On bigger documents each individual link export seems to update the document
leading to corrupt exports

src/apps-script/Code.js
@@ -480,9 +480,10 @@ exposed.insertField = function(field) {
 exposed.exportDocument = function(_, importInstructions) {
 	// Convert fields
 	var fields = getFields();
-	fields.forEach(function(field) {
+	for (var i = fields.length-1; i >= 0; i--) {
+		var field = fields[i];
 		field.write({text: field.code}, true);
-	});
+	}
 	var para;
 	var body = doc.getBody();
 	// Append document data
@@ -518,14 +519,15 @@ exposed.importDocument = function() {
 		},
 	}
 	var links = getAllLinks(true);
-	links.forEach(function(link) {
+	for (var i = links.length-1; i >= 0; i--) {
+		var link = links[i];
 		var text = link.text.getText().substring(link.startOffset, link.endOffsetInclusive+1).trim();
 		for (var key in importTypes) {
 			if (text.indexOf(key) == 0) {
 				return importTypes[key](link, text);
 			}
 		}
-	});
+	}
 	var paragraphs = doc.getBody().getParagraphs();
 	// Empty para
 	paragraphs[3].removeFromParent();

commit a6c915c6b80049712e9589396e76af088bfab265

Fix import breakage from 117206a

src/apps-script/Code.js
@@ -524,7 +524,7 @@ exposed.importDocument = function() {
 		var text = link.text.getText().substring(link.startOffset, link.endOffsetInclusive+1).trim();
 		for (var key in importTypes) {
 			if (text.indexOf(key) == 0) {
-				return importTypes[key](link, text);
+				importTypes[key](link, text);
 			}
 		}
 	}