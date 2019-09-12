JabRef/JabRef-Browser-Extension

commit 5460cb7f7ed3a91f5c4ecd5f4d0dd571c98fb4ff

Fix field editing
A recent change in Google Docs Kix editor broke field editing by making
the linkbubble disappear on more blur events

src/connector/client.js
@@ -195,6 +195,9 @@ Zotero.GoogleDocs.Client.prototype = {
 				}
 				throw e;
 			}
+			if (this.queued.insert) {
+				this.fields.splice(this.insertIdx, 0, this.queued.insert);
+			}
 			this.queued.insert = null;
 			this.queued.documentData = null;
 			this.queued.bibliographyStyle = null;

src/connector/ui.jsx
@@ -512,10 +512,10 @@ Zotero.GoogleDocs.UI = {
 	getSelectedFieldID: function() {
 		var linkbubble = document.querySelector('.docs-bubble.docs-linkbubble-bubble');
 		if (!linkbubble) return null;
-		var isZoteroLink = linkbubble.children[0].innerText.indexOf(Zotero.GoogleDocs.config.fieldURL) == 0;
-		var isLinkbubbleVisible = linkbubble.style.display == 'none';
-		if (isLinkbubbleVisible || !isZoteroLink) return null;
-		return linkbubble.children[0].innerText.substr(Zotero.GoogleDocs.config.fieldURL.length);
+		let linkbubbleText = Zotero.Utilities.trim(linkbubble.children[0].innerText);
+		var isZoteroLink = linkbubbleText.indexOf(Zotero.GoogleDocs.config.fieldURL) == 0;
+		if (!isZoteroLink) return null;
+		return linkbubbleText.substr(Zotero.GoogleDocs.config.fieldURL.length);
 	},
 
 	// Wait for google docs to save the text insertion

commit ede1f1e625e78377f7f0417b3db9e8e961ae287e

Properly fixes citation editing
5460cb7f broke new citation insertion

src/connector/client.js
@@ -56,18 +56,20 @@ Zotero.GoogleDocs = {
 			await Zotero.Connector_Browser.injectScripts(['zotero-google-docs-integration/ui.js']);
 		}
 		Zotero.GoogleDocs.UI.init();
-		window.addEventListener(`${Zotero.GoogleDocs.name}.call`, function(e) {
+		window.addEventListener(`${Zotero.GoogleDocs.name}.call`, async function(e) {
 			var client = Zotero.GoogleDocs.clients[e.data.client.id];
 			if (!client) {
 				client = new Zotero.GoogleDocs.Client();
+				await client.init();
 			}
 			client.call.apply(client, e.data.args);
 		});
 	},
 	
-	execCommand: function(command, client) {
+	execCommand: async function(command, client) {
 		if (!client) {
 			client = new Zotero.GoogleDocs.Client();
+			await client.init();
 		}
 		window.dispatchEvent(new MessageEvent('Zotero.Integration.execCommand', {
 			data: {client: {documentID: client.documentID, name: Zotero.GoogleDocs.name, id: client.id}, command}
@@ -84,6 +86,7 @@ Zotero.GoogleDocs = {
 	editField: async function() {
 		// Use the last client with a cached field list to speed up the cursorInField() lookup
 		var client = this.lastClient || new Zotero.GoogleDocs.Client();
+		await client.init();
 		try {
 			var field = await client.cursorInField();
 		} catch (e) {
@@ -113,6 +116,11 @@ Zotero.GoogleDocs.Client = function() {
 	Zotero.GoogleDocs.clients[this.id] = this;
 };
 Zotero.GoogleDocs.Client.prototype = {
+	init: async function() {
+		this.currentFieldID = await Zotero.GoogleDocs.UI.getSelectedFieldID();
+		this.isInLink = Zotero.GoogleDocs.UI.isInLink();
+	},
+	
 	call: async function(request) {
 		var method = request.command.split('.')[1];
 		var args = Array.from(request.arguments);
@@ -298,13 +306,11 @@ Zotero.GoogleDocs.Client.prototype = {
 	},
 	
 	cursorInField: async function() {
-		if (!Zotero.GoogleDocs.UI.getSelectedFieldID()) return false;
-		var selectedFieldID = Zotero.GoogleDocs.UI.getSelectedFieldID();
-		if (!selectedFieldID) return false;
+		if (!(this.currentFieldID)) return false;
 		
 		var fields = await this.getFields();
 		// The call to getFields() might change the selectedFieldID if there are duplicates
-		selectedFieldID = Zotero.GoogleDocs.UI.getSelectedFieldID();
+		let selectedFieldID = this.currentFieldID = await Zotero.GoogleDocs.UI.getSelectedFieldID();
 		for (let field of fields) {
 			if (field.id == selectedFieldID) {
 				return field;
@@ -314,7 +320,7 @@ Zotero.GoogleDocs.Client.prototype = {
 	},
 	
 	canInsertField: async function() {
-		return !Zotero.GoogleDocs.UI.isInLink();
+		return !this.isInLink;
 	},
 	
 	convert: async function(fieldIDs, fieldType, fieldNoteTypes) {

src/connector/ui.jsx
@@ -237,7 +237,6 @@ Zotero.GoogleDocs.UI = {
 	toggleUpdatingScreen: function(display) {
 		this.isUpdating = display || !this.isUpdating;
 		Zotero.GoogleDocs.UI.pleaseWaitScreen.toggle(this.isUpdating);
-		Zotero.GoogleDocs.UI.toggleEnabled(!this.isUpdating);
 	},
 	
 	initModeMonitor: async function() {
@@ -486,7 +485,7 @@ Zotero.GoogleDocs.UI = {
 	},
 	
 	moveCursorToEndOfCitation: async function() {
-		let selectedFieldID = this.getSelectedFieldID();
+		let selectedFieldID = await this.getSelectedFieldID(true);
 		if (selectedFieldID) {
 			let observer;
 			// Wait until doc changes are received from the GDocs backend
@@ -503,18 +502,27 @@ Zotero.GoogleDocs.UI = {
 				});
 			});
 			observer.disconnect();
+			selectedFieldID = await this.getSelectedFieldID(true);
+			if (!selectedFieldID) return;
 			await this.clickElement(document.getElementById('insertLinkButton'));
 			await Zotero.Promise.delay();
 			await this.clickElement(document.getElementsByClassName('docs-link-insertlinkbubble-buttonbar')[0].children[0]);
 		}
 	},
 	
-	getSelectedFieldID: function() {
-		var linkbubble = document.querySelector('.docs-bubble.docs-linkbubble-bubble');
+	getSelectedFieldID: async function(noAddRemove) {
+		// We add and remove a non-breaking-space to re-display the linkbubble in case it is not showing
+		// due to window blur or any other reasons.
+		if (!noAddRemove) {
+			await this.sendKeyboardEvent({key: " ", keyCode: 160});
+			await this.sendKeyboardEvent({key: "Backspace", keyCode: 8});
+		}
+		let linkbubble = document.querySelector('.docs-bubble.docs-linkbubble-bubble');
 		if (!linkbubble) return null;
 		let linkbubbleText = Zotero.Utilities.trim(linkbubble.children[0].innerText);
-		var isZoteroLink = linkbubbleText.indexOf(Zotero.GoogleDocs.config.fieldURL) == 0;
-		if (!isZoteroLink) return null;
+		let isZoteroLink = linkbubbleText.indexOf(Zotero.GoogleDocs.config.fieldURL) == 0;
+		let cursorInLink = this.isInLink();
+		if (!cursorInLink || !isZoteroLink) return null;
 		return linkbubbleText.substr(Zotero.GoogleDocs.config.fieldURL.length);
 	},
 
@@ -649,29 +657,32 @@ Zotero.GoogleDocs.UI.LinkbubbleOverride = class extends React.Component {
 		this.linkbubble = await this.waitForLinkbubble();
 		this.lastTop = "";
 		let style = this.linkbubble.style;
-		let open = style.display != 'none';
-		let url = this.linkbubble.children[0].innerText;
+		const url = this.linkbubble.children[0].innerText;
+		const open = style.display != 'none';
+
+		Zotero.GoogleDocs.UI.inLink = open;
+		Zotero.GoogleDocs.UI.lastLinkURL = url;
+		
 		// Check if on zotero field link
 		if (url.includes(Zotero.GoogleDocs.config.fieldURL)) {
 			this.setState({open});
 		}
-		Zotero.GoogleDocs.UI.lastLinkURL = url;
 		
 		this.observer = new MutationObserver(function(mutations) {
 			for (let mutation of mutations) {
 				if (mutation.attributeName != 'style') continue;
 
-				let url = this.linkbubble.children[0].innerText;
+				let style = this.linkbubble.style;
+				const url = this.linkbubble.children[0].innerText;
+				const open = style.display != 'none';
+
+				Zotero.GoogleDocs.UI.inLink = open;
+				Zotero.GoogleDocs.UI.lastLinkURL = url;
+				
 				// Check if on zotero field link
 				if (!url.includes(Zotero.GoogleDocs.config.fieldURL)) {
 					return this.setState({open: false});
 				}
-				
-				let style = this.linkbubble.style;
-				let open = style.display != 'none';
-				
-				Zotero.GoogleDocs.UI.inLink = open;
-				Zotero.GoogleDocs.UI.lastLinkURL = url;
 
 				// This is us moving the linkbubble away from view
 				if (this.state.open == open &&