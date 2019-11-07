61be7c3615240ddb0b43b113baff9074eebbb6a5
Galileo Sartor <galileo.sartor@gmail.com>
2019-11-07 12:11:33 +0100

    Add script to install host info on chromium

---
+++ install_linux.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+set -e
+
+DEBUG=false
+INSTALL_FILE="org.jabref.jabref.json"
+
+askBrowserSnap() {
+    if (whiptail --title "Snap Choice" --defaultno \
+            --yesno "Is this browser installed as a snap (usually NO)?" 8 60); then
+        # BASE_DIR="$1"
+        whiptail --title "Snap Choice" --msgbox "Sorry, browsers installed as snaps are not supported at this time" 8 50
+        exit 0
+    fi
+}
+
+getJson() {
+    if [[ ! -z $1 ]]; then
+        # Insert Firefox data
+        JSON_OUT="https://raw.githubusercontent.com/JabRef/jabref/master/buildres/linux/native-messaging-host/firefox/org.jabref.jabref.json"
+    else
+        # Insert Chrome data
+        JSON_OUT="https://raw.githubusercontent.com/JabRef/jabref/master/buildres/linux/native-messaging-host/chromium/org.jabref.jabref.json"
+    fi
+}
+
+setupFirefox() {
+    askBrowserSnap "./snap/firefox/common"
+    getJson "firefox"
+    INSTALL_USER="$HOME/.mozilla/native-messaging-hosts"
+    INSTALL_ROOT="/usr/lib/mozilla/native-messaging-hosts"
+}
+
+setupChrome() {
+    getJson
+    INSTALL_USER="$HOME/.config/google-chrome/NativeMessagingHosts"
+    INSTALL_ROOT="/etc/opt/chrome/native-messaging-hosts"
+}
+
+setupChromium() {
+    askBrowserSnap "./snap/chromium/current"
+    getJson
+    INSTALL_USER="$HOME/.config/chromium/NativeMessagingHosts"
+    INSTALL_ROOT="/etc/chromium/native-messaging-hosts"
+}
+
+setupVivaldi() {
+    getJson
+    INSTALL_USER="$HOME/.config/vivaldi/NativeMessagingHosts"
+    INSTALL_ROOT="/etc/vivaldi/native-messaging-hosts"
+}
+
+setupBrave() {
+    getJson
+    INSTALL_USER="$HOME/.config/BraveSoftware/Brave-Browser/NativeMessagingHosts"
+    INSTALL_ROOT="/etc/chromium/native-messaging-hosts"
+}
+
+# --------------------------------
+# Start of script
+# --------------------------------
+
+BROWSER=$(whiptail \
+            --title "Browser Selection" \
+            --menu "Choose a browser to integrate with KeePassXC:" \
+            15 60 5 \
+            "1" "Firefox" \
+            "2" "Chrome" \
+            "3" "Chromium" \
+            "4" "Vivaldi" \
+            "5" "Brave" \
+            "6" "Opera" \
+            3>&1 1>&2 2>&3)
+
+clear
+
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    # Configure settings for the chosen browser
+    case "$BROWSER" in
+        1) setupFirefox ;;
+        2) setupChrome ;;
+        3) setupChromium ;;
+        4) setupVivaldi ;;
+        5) setupBrave ;;
+        6) setupChrome ;;
+    esac
+
+    if [[ $EUID != 0 ]]; then
+        # User-based install (does not require sudo premission)
+        if (whiptail --title "User Install" --yesno "User-based installation in progress for user $USER...\nNote: use sudo ./install_linux.sh for global installation. Proceed?" 8 78); then
+            INSTALL_DIR=$INSTALL_USER
+        else
+            exit 1
+        fi
+    else
+        if (whiptail --title "Root Install" --yesno "Root Installation in progress all users... Proceed?"); then
+            INSTALL_DIR=$INSTALL_ROOT
+        else
+            exit 1
+        fi
+    fi
+
+
+
+    mkdir -p "$INSTALL_DIR"
+    curl -SL --silent "$JSON_OUT" -o ${INSTALL_DIR}/${INSTALL_FILE}
+
+    $DEBUG && echo "Installed to: ${INSTALL_DIR}/${INSTALL_FILE}"
+
+    whiptail \
+        --title "Installation Complete" \
+        --msgbox "You will need to restart your browser in order to connect to KeePassXC" \
+        8 50
+else
+    whiptail --title "Installation Canceled" --msgbox "No changes were made to your system" 8 50
+fi
