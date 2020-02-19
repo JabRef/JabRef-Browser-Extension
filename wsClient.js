d4f38b4047b17abad8dd69d2f408bb35b5c7b07f
systemoperator <3658393+systemoperator@users.noreply.github.com>
2020-02-19 22:01:18 +0100

    wsClient.js: extension, refactoring

--- wsClient.js
+++ wsClient.js
@@ -1,57 +1,200 @@
 'use strict';
 
+/*
+ * A simple, robust websocket client implementation for the JabRef-Browser-Extension for bidirectional communication.
+ */
 let wsClient = {
-    _connection: null,
-    _pingTimeout: null,
+    WsAction: {
+        // receive only
+        HEARTBEAT: "heartbeat",
+        INFO_CONFIGURATION: "info.configuration",
+        CMD_FETCH_GOOGLE_SCHOLAR_CITATION_COUNTS: "cmd.fetchGoogleScholarCitationCounts",
+        CMD_CONTINUE_FETCH_GOOGLE_SCHOLAR_CITATION_COUNTS: "cmd.continueFetchGoogleScholarCitationCounts",
+
+        // send only
+        CMD_REGISTER: "cmd.register",
+        INFO_GOOGLE_SCHOLAR_CITATION_COUNTS: "info.googleScholarCitationCounts",
+        INFO_FETCH_GOOGLE_SCHOLAR_CITATION_COUNTS_INTERRUPTED: "info.fetchGoogleScholarCitationCountsInterrupted",
+
+        // send and receive
+        INFO_MESSAGE: "info.message"
+    },
+
+    WsClientType: {
+        UNKNOWN: "unknown",
+        JABREF_BROWSER_EXTENSION: "JabRefBrowserExtension"
+    },
+
+    // configuration (must be configured before starting the client)
+    configuration: {
+        websocketScheme: "ws", // "ws" or "wss"
+        websocketHost: "localhost", // "localhost", "127.0.0.1", ...
+        websocketPort: "8855",
+
+        heartbeatEnabled: true,
+        heartbeatInterval: 6000, // [ms] should be an even number
+        heartbeatToleranceFactor: 0.5,
+
+        tryReconnect: true // true: tries to reconnect, if not connected
+    },
+
+    tryReconnectFlag: false,
+    wsReconnectInterval: 5000, // [ms]
+
+    connection: null,
+    heartbeatTimeout: null, // timeout instance
+
+    getConfiguration: function () {
+        return wsClient.configuration;
+    },
+
+    /**
+     * should be called before starting the websocket client
+     * @param configuration
+     */
+    setConfiguration: function (configuration) {
+        wsClient.configuration = configuration;
+    },
 
     heartbeat: function () {
-        clearTimeout(this._pingTimeout);
+        console.log("[ws] heartbeat called...");
 
-        // Delay should be equal to the interval at which the server
-        // sends out pings plus a conservative assumption of the latency.
-        this._pingTimeout = setTimeout(() => {
-            //this._connection.close();
-        }, 30000 + 1000);
+        clearTimeout(wsClient.heartbeatTimeout);
+
+        if (wsClient.configuration.heartbeatEnabled) {
+
+            wsClient.heartbeatTimeout = setTimeout(() => {
+                console.log("[ws] closing websocket (reason: no heartbeat in time)");
+                wsClient.connection.close();
+            }, wsClient.configuration.heartbeatInterval * (1 + wsClient.configuration.heartbeatToleranceFactor));
+        }
     },
 
-    send: function (message) {
-        //this._connection.send(JSON.stringify(message);
-        this._connection.send(message);
+    sendMessage: function (wsAction, messagePayload) {
+        let messageContainer = {};
+        messageContainer.action = wsAction;
+        messageContainer.payload = messagePayload;
+
+        wsClient.connection.send(JSON.stringify(messageContainer));
     },
 
-    close: function (message) {
-        this._connection.close();
+    closeConnection: function () {
+        wsClient.tryReconnectFlag = false; // disable trying to reconnect, if closing connection was triggered manually
+        wsClient.connection.close();
     },
 
     init: function () {
+        if (!("WebSocket" in window)) {
+            console.log("[ws] Websockets are not supported by this browser! Please use another browser.");
 
-        wsClient._connection = new WebSocket("ws://localhost:8855");
+            return;
+        }
 
-        this._connection.onopen = function (event) {
-            console.log("[open] Connection established");
-            console.log("Sending to server");
-            wsClient.heartbeat();
-            wsClient.send("My name is John");
+        wsClient.tryReconnectFlag = wsClient.configuration.tryReconnect;
+
+        wsClient.connection = new WebSocket(wsClient.configuration.websocketScheme + "://" + wsClient.configuration.websocketHost + ":" + wsClient.configuration.websocketPort);
+
+        wsClient.connection.onopen = function (event) {
+            console.log("[ws] @onOpen: connection established");
+
+            if (wsClient.configuration.heartbeatEnabled) {
+                wsClient.heartbeat();
+            }
+
+            let messagePayload = {};
+            messagePayload.wsClientType = wsClient.WsClientType.JABREF_BROWSER_EXTENSION;
+
+            wsClient.sendMessage(wsClient.WsAction.CMD_REGISTER, messagePayload);
         };
 
-        this._connection.onclose = function (event) {
-            clearTimeout(wsClient._pingTimeout);
+        wsClient.connection.onclose = function (event) {
+            // onclose() will be called:
+            // - in case the connection gets closed properly
+            // - after onerror() [if the underlying websocket is implemented properly]
+            // - after unsuccessful connection attempt
+            // - see:
+            //   - https://stackoverflow.com/questions/40084398/is-onclose-always-called-after-onerror-for-websocket
+            //   - https://html.spec.whatwg.org/multipage/web-sockets.html
+
             if (event.wasClean) {
-                console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+                console.log(`[ws] @onClose: connection closed cleanly (code="${event.code}", reason="${event.reason}")`);
             } else {
                 // e.g. server process killed or network down
                 // event.code is usually 1006 in this case
-                console.log('[close] Connection died');
+                console.log(`[ws] @onClose: connection died (code="${event.code}", reason="${event.reason}")`);
+            }
+
+            clearTimeout(wsClient.heartbeatTimeout);
+
+            if (wsClient.tryReconnectFlag) {
+                setTimeout(() => {
+                    console.log("[ws] trying to connect...");
+                    wsClient.init();
+                }, wsClient.wsReconnectInterval);
             }
         };
 
-        this._connection.onerror = function (event) {
-            console.log(`[error] ${event.message}`);
-            clearTimeout(wsClient._pingTimeout);
+        wsClient.connection.onerror = function (event) {
+            console.log(`[ws] @onError`);
+            clearTimeout(wsClient.heartbeatTimeout);
         };
 
-        this._connection.onmessage = function (event) {
-            console.log(`[message] Data received from server: ${event.data}`);
+        wsClient.connection.onmessage = function (event) {
+            console.log(`[ws] @onMessage: ${event.data}`);
+
+            let messageContainer = JSON.parse(event.data);
+
+            let action = messageContainer.action;
+            let messagePayload = messageContainer.payload;
+
+            if (!Object.values(wsClient.WsAction).includes(action)) {
+                console.log("[ws] unknown WsAction received: " + action);
+                return;
+            }
+
+            if (action === wsClient.WsAction.HEARTBEAT) {
+                wsClient.handlerHeartbeat(messagePayload);
+            } else if (action === wsClient.WsAction.INFO_CONFIGURATION) {
+                wsClient.handlerInfoConfiguration(messagePayload);
+            } else if (action === wsClient.WsAction.INFO_MESSAGE) {
+                wsClient.handlerInfoMessage(messagePayload);
+            } else if (action === wsClient.WsAction.CMD_FETCH_GOOGLE_SCHOLAR_CITATION_COUNTS) {
+                wsClient.handlerCmdFetchGoogleScholarCitationCounts(messagePayload);
+            } else if (action === wsClient.WsAction.CMD_CONTINUE_FETCH_GOOGLE_SCHOLAR_CITATION_COUNTS) {
+                wsClient.handlerCmdContinueFetchGoogleScholarCitationCounts(messagePayload);
+            } else {
+                console.log("[ws] unimplemented WsAction received: " + action);
+            }
         };
-    }
+    },
+
+    handlerHeartbeat: function (messagePayload) {
+        if (wsClient.configuration.heartbeatEnabled) {
+            wsClient.heartbeat();
+        }
+    },
+
+    handlerInfoConfiguration: function (messagePayload) {
+        wsClient.configuration.heartbeatEnabled = messagePayload.heartbeatEnabled;
+        wsClient.configuration.heartbeatInterval = messagePayload.heartbeatInterval;
+        wsClient.configuration.heartbeatToleranceFactor = messagePayload.heartbeatToleranceFactor;
+
+        if (wsClient.configuration.heartbeatEnabled) {
+            wsClient.heartbeat();
+        } else {
+            clearTimeout(wsClient.heartbeatTimeout);
+        }
+    },
+
+    handlerInfoMessage: function (messagePayload) {
+        console.log("[ws] " + messagePayload.messageType + ": " + messagePayload.message);
+    },
+
+    handlerCmdFetchGoogleScholarCitationCounts: function (messagePayload) {
+
+    },
+
+    handlerCmdContinueFetchGoogleScholarCitationCounts: function (messagePayload) {
+
+    },
 };
