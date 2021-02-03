JabRef/JabRef-Browser-Extension

commit 2ffbb4f60abe239e0ca55914ef13241c4e1b2b8f

Fix potential race condition when moving cursor out of citation
https://forums.zotero.org/discussion/87314/google-docs-citations-stuck-on-updating
https://forums.zotero.org/discussion/87304/footnotes-stuck-as-updating-312151190#latest
and others

src/connector/ui.jsx
@@ -563,6 +563,15 @@ Zotero.GoogleDocs.UI = {
 	},
 	
 	moveCursorToEndOfCitation: async function() {
+		var selection = document.querySelector('.docs-texteventtarget-iframe').contentDocument.body.textContent;
+		if (selection.length) {
+			var textEventTarget = document.querySelector('.docs-texteventtarget-iframe').contentDocument;
+			textEventTarget.dispatchEvent(new KeyboardEvent('keydown', {key: "ArrowRight", keyCode: 39}));
+			await Zotero.Promise.delay();
+		}
+		await this.sendKeyboardEvent({key: " ", keyCode: 160});
+		await this.sendKeyboardEvent({key: "Backspace", keyCode: 8});
+		await this.waitToSaveInsertion();
 		return this.getSelectedFieldID();
 	},
 	

commit 2d8fbf6d3c60057122a8732c4014c3f02efdfaa4

Fix insertions on Mac Edge resulting in plaintext citations
Regression from 2ffbb4f6.

We're trying to find a way to trigger the google docs sync icon after
running an apps script command in the background so we're sure we have
up-to-date text before attempting to move the cursor to the end of the
citation. Adding and removing a character at the wrong time (especially
if you add a spacebar before receiving apps-script updates and remove it
after) seems to remove the link in text.

This instead triggers the bold text modifier on and off, which triggers
gdocs sync without messing up the link in text.

src/connector/ui.jsx
@@ -569,8 +569,14 @@ Zotero.GoogleDocs.UI = {
 			textEventTarget.dispatchEvent(new KeyboardEvent('keydown', {key: "ArrowRight", keyCode: 39}));
 			await Zotero.Promise.delay();
 		}
-		await this.sendKeyboardEvent({key: " ", keyCode: 160});
-		await this.sendKeyboardEvent({key: "Backspace", keyCode: 8});
+		let event = {key: "b", keyCode: 66};
+		if (Zotero.isMac) {
+			event.metaKey = true
+		} else {
+			event.ctrlKey = true;
+		}
+		await this.sendKeyboardEvent(event);
+		await this.sendKeyboardEvent(event);
 		await this.waitToSaveInsertion();
 		return this.getSelectedFieldID();
 	},