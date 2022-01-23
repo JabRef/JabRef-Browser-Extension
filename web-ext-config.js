ce08530d81b226bf9315175c2f090623e185fd1a
Tobias Diez <code@tobiasdiez.com>
2022-01-23 18:26:49 +0100

    Update files to exclude from release

--- web-ext-config.js
+++ web-ext-config.js
@@ -1,24 +1,23 @@
 module.exports = {
-	ignoreFiles: [
-		'zotero-connectors',
-		'circle.yml',
-		'gulpfile.js',
-		'JabFox.sublime-project',
-		'JabFox.sublime-workspace',
-		'package-lock.json',
-		'install_linux.sh',
-		'zotero-scholar-citations',
-		'.idea'
-	],
-	run: {
-		startUrl: [
-			'about:debugging',
-			'https://ieeexplore.ieee.org/abstract/document/893874',
-			'https://arxiv.org/a/diez_t_1.html',
-		],
-		browserConsole: true,
-	},
-	build: {
-		overwriteDest: true
-	}
-};
+    ignoreFiles: [
+        'zotero-connectors',
+        'circle.yml',
+        'gulpfile.js',
+        'package-lock.json',
+        'install_linux.sh',
+        'zotero-scholar-citations',
+        '.idea',
+        'workspace.code-workspace',
+    ],
+    run: {
+        startUrl: [
+            'about:debugging',
+            'https://ieeexplore.ieee.org/abstract/document/893874',
+            'https://arxiv.org/a/diez_t_1.html',
+        ],
+        browserConsole: true,
+    },
+    build: {
+        overwriteDest: true,
+    },
+}
