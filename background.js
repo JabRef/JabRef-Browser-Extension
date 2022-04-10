5c64e2a439891edb1c1ed848ee328605065d209c
JS Moore <jakkarth@ignisobscurae.com>
2022-04-10 11:18:44 -0400

    Add a visual indicator to the import action icon for whether extended citation data has been provided by a translator or not.

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 /*
-	Initialize
+    Initialize
 */
 Zotero.Debug.init(1)
 Zotero.Repo.init()
@@ -12,7 +12,7 @@ zsc.init()
 this.tabInfo = new Map()
 
 /*
-	Show/hide import button for all tabs (when add-on is loaded).
+    Show/hide import button for all tabs (when add-on is loaded).
 */
 browser.tabs.query({}).then((tabs) => {
     // We wait a bit before injection to give Zotero time to load the translators
@@ -25,7 +25,7 @@ browser.tabs.query({}).then((tabs) => {
 })
 
 /*
-	Show/hide import button for the currently active tab, whenever the user navigates.
+    Show/hide import button for the currently active tab, whenever the user navigates.
 */
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (!changeInfo.url) {
@@ -49,12 +49,12 @@ browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 })
 
 /*
-	Remove translator information when tab is closed.
+    Remove translator information when tab is closed.
 */
 browser.tabs.onRemoved.addListener(Zotero.Connector_Browser.onPageLoad)
 
 /*
-	Disable add-on for special browser pages
+    Disable add-on for special browser pages
 */
 function isDisabledForURL(url) {
     return (
@@ -65,7 +65,7 @@ function isDisabledForURL(url) {
 }
 
 /*
-	Searches for translators for the given tab and shows/hides the import button accordingly.
+    Searches for translators for the given tab and shows/hides the import button accordingly.
 */
 function installInTab(tab) {
     if (isDisabledForURL(tab.url)) {
@@ -173,7 +173,12 @@ onTranslators = function (translators, tabId, contentType) {
             JSON.parse(JSON.stringify(tabId))
         )
         tabInfo.set(tabId, { ...tabInfo.get(tabId), hasTranslator: false })
-
+        browser.pageAction.setIcon({
+            tabId: tabId, path: {
+                "48": "data/JabRef-icon-48.png",
+                "96": "data/JabRef-icon-96.png"
+            }
+        })
         browser.pageAction.show(tabId)
         browser.pageAction.setTitle({
             tabId: tabId,
@@ -187,7 +192,12 @@ onTranslators = function (translators, tabId, contentType) {
             JSON.parse(JSON.stringify(tabId))
         )
         tabInfo.set(tabId, { ...tabInfo.get(tabId), hasTranslator: true })
-
+        browser.pageAction.setIcon({
+            tabId: tabId, path: {
+                "48": "data/JabRef-icon-plus-48.png",
+                "96": "data/JabRef-icon-plus-96.png"
+            }
+        })
         browser.pageAction.show(tabId)
         browser.pageAction.setTitle({
             tabId: tabId,
