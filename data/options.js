d56f8a9abece3e04ddffa0a79d90412f915f167d
Tobias Diez <code@tobiasdiez.de>
2026-01-31 22:28:51 +0100

    feat: add http connection check to options page

--- data/options.js
+++ data/options.js
@@ -3,8 +3,25 @@ var ExportMode = Object.freeze({
   BibTeX: "bibtex",
 });
 
-function checkConnections() {
-  let status = document.getElementById("connectionStatus");
+const DEFAULT_PORT = 23119;
+
+async function connectToJabRef(port) {
+  base = `http://localhost:${port}/`;
+  try {
+    // Try a simple GET to the base URL to detect availability.
+    const response = await fetch(base, { method: "GET", cache: "no-store" });
+    if (response && (response.ok || response.status === 404)) {
+      return { success: true };
+    } else {
+      return { success: false, status: response.status };
+    }
+  } catch (error) {
+    return { success: false, error: error };
+  }
+}
+
+function checkConnections({ httpPort }) {
+  let status = document.getElementById("connectionStatusNative");
   browser.runtime
     .sendNativeMessage("org.jabref.jabref", {
       status: "validate",
@@ -26,64 +43,62 @@ function checkConnections() {
       status.textContent = error.message;
     });
 
-  let wsClientStatus = document.getElementById("wsClientStatus");
-  browser.runtime
-    .sendMessage({
-      getWsClientState: true,
-    })
-    .then((response) => {
-      if (response.clientStarted) {
-        switch (response.connectionState) {
-          case WebSocket.CONNECTING:
-            wsClientStatus.setAttribute("class", "alert-positive");
-            wsClientStatus.textContent = "connecting";
-            break;
-          case WebSocket.OPEN:
-            wsClientStatus.setAttribute("class", "alert-positive");
-            wsClientStatus.textContent = "connected";
-            break;
-          case WebSocket.CLOSING:
-            wsClientStatus.setAttribute("class", "alert-error");
-            wsClientStatus.textContent = "closing";
-            break;
-          case WebSocket.CLOSED:
-            wsClientStatus.setAttribute("class", "alert-error");
-            wsClientStatus.textContent = "closed";
-            break;
-          default:
-            wsClientStatus.setAttribute("class", "alert-error");
-            wsClientStatus.textContent = "n/a";
+  let httpStatus = document.getElementById("connectionStatusHttp");
+  httpStatus.textContent = `Testing connection on port ${httpPort}...`;
+  connectToJabRef(httpPort).then((result) => {
+    if (result.success) {
+      httpStatus.setAttribute("class", "alert-positive");
+      httpStatus.textContent = `JabRef reachable on port ${httpPort}`;
+    } else {
+      httpStatus.setAttribute("class", "alert-error");
+      if (result.status) {
+        httpStatus.textContent = `Errors with status code "${result.status}"`;
+      } else if (result.error) {
+        httpStatus.textContent = `Connection error: ${result.error.message}`;
+        if (
+          result.error instanceof TypeError &&
+          (result.error.message.includes("Failed to fetch") ||
+            result.error.message.includes("NetworkError"))
+        ) {
+          httpStatus.textContent += `\nConnection to port ${httpPort} failed. Is JabRef running and configured to accept HTTP connections on this port?`;
         }
       } else {
-        wsClientStatus.setAttribute("class", "alert-error");
-        wsClientStatus.textContent = "Websocket client not started";
+        httpStatus.textContent = `Connection to port ${httpPort} failed for unknown reasons.`;
       }
-    });
+    }
+  });
 }
 
-function restoreOptions() {
-  browser.storage.sync
-    .get({ exportMode: ExportMode.BibTeX, takeSnapshots: false, retrieveCitationCounts: false })
-    .then((res) => {
-      console.log("exportMode = " + res.exportMode);
-      console.log("takeSnapshots = " + res.takeSnapshots);
-      console.log("retrieveCitationCounts = " + res.retrieveCitationCounts);
-      if (res.exportMode === ExportMode.BibLaTeX) {
-        document.getElementById("exportBiblatex").checked = true;
-      } else {
-        document.getElementById("exportBibtex").checked = true;
-      }
-      if (res.takeSnapshots) {
-        document.getElementById("snapshotsOn").checked = true;
-      } else {
-        document.getElementById("snapshotsOff").checked = true;
-      }
-      if (res.retrieveCitationCounts) {
-        document.getElementById("retrieveCitationCountsOn").checked = true;
-      } else {
-        document.getElementById("retrieveCitationCountsOff").checked = true;
-      }
-    });
+async function restoreOptions() {
+  const options = await browser.storage.sync.get({
+    exportMode: ExportMode.BibTeX,
+    takeSnapshots: false,
+    retrieveCitationCounts: false,
+    httpPort: DEFAULT_PORT,
+  });
+
+  console.log("exportMode = " + options.exportMode);
+  console.log("takeSnapshots = " + options.takeSnapshots);
+  console.log("retrieveCitationCounts = " + options.retrieveCitationCounts);
+  console.log("httpPort = " + options.httpPort);
+  if (options.exportMode === ExportMode.BibLaTeX) {
+    document.getElementById("exportBiblatex").checked = true;
+  } else {
+    document.getElementById("exportBibtex").checked = true;
+  }
+  if (options.takeSnapshots) {
+    document.getElementById("snapshotsOn").checked = true;
+  } else {
+    document.getElementById("snapshotsOff").checked = true;
+  }
+  if (options.retrieveCitationCounts) {
+    document.getElementById("retrieveCitationCountsOn").checked = true;
+  } else {
+    document.getElementById("retrieveCitationCountsOff").checked = true;
+  }
+
+  document.getElementById("portInput").value = options.httpPort;
+  return options;
 }
 
 function saveOptions() {
@@ -95,16 +110,20 @@ function saveOptions() {
   }
   var takeSnapshots = document.getElementById("snapshotsOn").checked;
   var retrieveCitationCounts = document.getElementById("retrieveCitationCountsOn").checked;
-  browser.storage.sync.set({
+  var port = document.getElementById("portInput").value;
+  const options = {
     exportMode: exportMode,
     takeSnapshots: takeSnapshots,
     retrieveCitationCounts: retrieveCitationCounts,
-  });
+    httpPort: port,
+  };
+  browser.storage.sync.set(options);
+  return options;
 }
 
-function init() {
-  restoreOptions();
-  checkConnections();
+async function init() {
+  const options = await restoreOptions();
+  checkConnections(options);
 
   document.getElementById("exportBiblatex").addEventListener("change", () => saveOptions());
   document.getElementById("exportBibtex").addEventListener("change", () => saveOptions());
@@ -118,6 +137,19 @@ function init() {
   document
     .getElementById("retrieveCitationCountsOff")
     .addEventListener("change", () => saveOptions());
+
+  const portInput = document.getElementById("portInput");
+  const portChanged = () => {
+    const options = saveOptions();
+    checkConnections(options);
+  };
+  portInput.addEventListener("change", () => portChanged());
+  portInput.addEventListener("blur", () => portChanged());
+  portInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      portChanged();
+    }
+  });
 }
 
 document.addEventListener("DOMContentLoaded", init);
