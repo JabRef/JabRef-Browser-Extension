a131530fc80fc11a43f39a3fe3cbd5a4d8c1b473
Siedlerchr <siedlerkiller@gmail.com>
2026-06-21 19:36:12 +0200

    warnigns

--- wxt.config.ts
+++ wxt.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
       },
     },
     commands: {
-      _execute_page_action: {
+      _execute_action: {
         suggested_key: {
           default: "Alt+Shift+J",
         },
@@ -26,10 +26,6 @@ export default defineConfig({
     },
     description:
       "The JabRef browser extension imports new bibliographic information directly from the browser into JabRef.",
-    developer: {
-      name: "JabRef",
-      url: "http://www.jabref.org/",
-    },
     homepage_url: "http://www.jabref.org/",
     host_permissions: ["<all_urls>"],
     icons: {
@@ -41,7 +37,6 @@ export default defineConfig({
     key: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAimiMLZZCsf+p92UUzQRWYljtoUk0a9AuN+D3TJTFcm1BEDXKIDVmWG20S4yLQyYs8kWao3eTSdYykgsZLPtay1pFKtoM4csGB6sEOO+h25Nv/AU7pN5yH5PqcTIGkuH6AsQQQTPS1Y+vDfz+548oVXzK033l6ernhKRj4dngueZyQX89U38zkorq0/PPWfE8ppPzXiWo1Pn5C5scgzaHSfavIkbBpWuiJw6moSoYw4UxzmU6FmzjM/c8Ags/QPU/8M3BeC1eigStifBDkuIIDQtMtiTXEgCqHjIacB3uB7SJKL+0wsoREqoz3cX7uNLnB+DKu+s0OZKVah8gkliBLQIDAQAB",
     name: "JabRef Browser Extension",
     permissions: [
-      "<all_urls>",
       "scripting",
       "activeTab",
       "tabs",
